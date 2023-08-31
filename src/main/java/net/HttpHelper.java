e8e17d77b3a7fb610c0a5403a03bf6777baf54f3
VastCosmic <137017677@qq.com>
2023-08-31 16:18:48 +0800

    redesign

--- src/main/java/net/HttpHelper.java
+++ src/main/java/net/HttpHelper.java
@@ -1,5 +1,6 @@
 package net;
 
+import Log.Log;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.ClassicHttpRequest;
@@ -13,94 +14,91 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class HttpHelper {
     public static IResponse get(String url) throws IOException {
-        AtomicReference<IResponse> result = new AtomicReference<>(null);
-        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            ClassicHttpRequest httpGet = ClassicRequestBuilder.get(url)
-                    .build();
-            reqMethod(result, httpclient, httpGet);
-        }
-        return result.get();
+        ClassicHttpRequest httpGet = ClassicRequestBuilder.get(url).build();
+        return executeRequest(httpGet);
     }
-    // 自定义token
+
     public static IResponse get(String url, String token) throws IOException {
-        AtomicReference<IResponse> result = new AtomicReference<>(null);
-        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            ClassicHttpRequest httpGet = ClassicRequestBuilder.get(url).build();
-            //httpGet.setHeader("Authorization", "Bearer " + token);
-            httpGet.setHeader("Authorization", token);
-            reqMethod(result, httpclient, httpGet);
-        }
-        return result.get();
+        ClassicHttpRequest httpGet = ClassicRequestBuilder.get(url)
+                .setHeader("Authorization", token)
+                .build();
+        return executeRequest(httpGet);
     }
-    // 自定义请求头
+
     public static IResponse get(String url, Header header) throws IOException {
-        AtomicReference<IResponse> result = new AtomicReference<>(null);
-        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            ClassicHttpRequest httpGet = ClassicRequestBuilder.get(url).build();
-            httpGet.setHeader(header);
-            reqMethod(result, httpclient, httpGet);
-        }
-        return result.get();
+        ClassicHttpRequest httpGet = ClassicRequestBuilder.get(url)
+                .setHeader(header)
+                .build();
+        return executeRequest(httpGet);
     }
 
     public static IResponse post(String url, String reqBody) throws IOException {
-        if(reqBody == null) {
-            System.out.println("ReqBody can not be null!");
+        if (reqBody == null) {
+            Log.error("ReqBody can not be null!");
             return null;
         }
-        AtomicReference<IResponse> result = new AtomicReference<>(null);
-        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            ClassicHttpRequest httpPost = ClassicRequestBuilder.post(url)
-                    .setEntity(new StringEntity(reqBody))
-                    .build();
-            reqMethod(result, httpclient, httpPost);
-        }
-        return result.get();
+        ClassicHttpRequest httpPost = ClassicRequestBuilder.post(url)
+                .setEntity(new StringEntity(reqBody))
+                .build();
+        return executeRequest(httpPost);
     }
 
     public static IResponse post(String url, String reqBody, String token) throws IOException {
-        if(reqBody == null) {
-            System.out.println("ReqBody can not be null!");
+        if (reqBody == null) {
+            Log.error("ReqBody can not be null!");
             return null;
         }
-        AtomicReference<IResponse> result = new AtomicReference<>(null);
-        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            ClassicHttpRequest httpPost = ClassicRequestBuilder.post(url)
-                    .setEntity(new StringEntity(reqBody))
-                    .build();
-            //httpGet.setHeader("Authorization", "Bearer " + token);
-            httpPost.setHeader("Authorization", token);
-            reqMethod(result, httpclient, httpPost);
-        }
-        return result.get();
+        ClassicHttpRequest httpPost = ClassicRequestBuilder.post(url)
+                .setEntity(new StringEntity(reqBody))
+                .setHeader("Authorization", token)
+                .build();
+        return executeRequest(httpPost);
     }
 
     public static IResponse post(String url, String reqBody, Header header) throws IOException {
-        if(reqBody == null) {
-            System.out.println("ReqBody can not be null!");
+        if (reqBody == null) {
+            Log.error("ReqBody can not be null!");
             return null;
         }
+        ClassicHttpRequest httpPost = ClassicRequestBuilder.post(url)
+                .setEntity(new StringEntity(reqBody))
+                .setHeader(header)
+                .build();
+        return executeRequest(httpPost);
+    }
+
+    public static IResponse put(String url, String reqBody) {
+        if (reqBody == null) {
+            Log.error("ReqBody can not be null!");
+            return null;
+        }
+        ClassicHttpRequest httpPut = ClassicRequestBuilder.put(url)
+                .setEntity(new StringEntity(reqBody))
+                .build();
+        return executeRequest(httpPut);
+    }
+
+    public static IResponse delete(String url) throws IOException {
+        ClassicHttpRequest httpDelete = ClassicRequestBuilder.delete(url).build();
+        return executeRequest(httpDelete);
+    }
+
+    private static IResponse executeRequest(ClassicHttpRequest request) {
         AtomicReference<IResponse> result = new AtomicReference<>(null);
         try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            ClassicHttpRequest httpPost = ClassicRequestBuilder.post(url)
-                    .setEntity(new StringEntity(reqBody))
-                    .build();
-            //httpGet.setHeader("Authorization", "Bearer " + token);
-            httpPost.setHeader(header);
-            reqMethod(result, httpclient, httpPost);
+            httpclient.execute(request, response -> {
+                var code = response.getCode();
+                var reason = response.getReasonPhrase();
+                var version = response.getVersion();
+                var headers = response.getHeaders();
+                var body = EntityUtils.toString(response.getEntity());
+                result.set(new IResponse(code, reason, headers, body));
+                return true;
+            });
+        } catch (IOException e){
+            Log.error(e.getMessage());
         }
         return result.get();
     }
-
-    private static void reqMethod(AtomicReference<IResponse> result, CloseableHttpClient httpclient, ClassicHttpRequest httpRequest) throws IOException {
-        httpclient.execute(httpRequest, response -> {
-            var code = response.getCode();
-            var reason = response.getReasonPhrase();
-            var version = response.getVersion();
-            var headers = response.getHeaders();
-            var body = EntityUtils.toString(response.getEntity());
-            result.set(new IResponse(code, reason, headers, body));
-            return true;
-        });
-    }
 }
+
